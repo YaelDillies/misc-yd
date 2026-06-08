1ab070c9b8891604230dd773c1cbd2716a54de9f
Yaël Dillies <yael.dillies@gmail.com>
2026-06-08 18:11:35 +0200

    Remove material that was moved to MeanFourier/FormalConjectures

--- MiscYD.lean
+++ MiscYD.lean
@@ -11,17 +11,8 @@ public import MiscYD.Book.MetricBetween
 public import MiscYD.Book.SylvesterChvatal
 public import MiscYD.Convexity.MinkowskiCaratheodory
 public import MiscYD.Mathlib.Analysis.Convex.Combination
-public import MiscYD.Mathlib.Combinatorics.SetFamily.Shatter
 public import MiscYD.Mathlib.GroupTheory.OrderOfElement
 public import MiscYD.Mathlib.LinearAlgebra.AffineSpace.Combination
 public import MiscYD.Mathlib.LinearAlgebra.AffineSpace.FiniteDimensional
 public import MiscYD.Mathlib.MeasureTheory.MeasurableSpace.Basic
-public import MiscYD.PhD.VCDim.AddVCDim
-public import MiscYD.PhD.VCDim.Convex
-public import MiscYD.PhD.VCDim.HausslerPacking
-public import MiscYD.PhD.VCDim.HypercubeEdges
-public import MiscYD.PhD.VCDim.SmallAvDegImpExistsSmallOutDeg
-public import MiscYD.PhD.VCDim.SmallVCImpSmallCondVar
-public import MiscYD.PhD.VCDim.UnifAP.Defs
-public import MiscYD.PhD.VCDim.UnifAP.Fourier
 public import MiscYD.SetFamily.PosDiffs
